8bd36a4f89e748d39ca95c7bc7ee44c983525196
Mikael <mika3866@niels.brock.dk>
2019-03-04 14:03:28 +0100

    finish

---
+++ java.js
@@ -0,0 +1,20 @@
+
+function myFunction1() {
+  document.getElementById("clickme1").innerHTML = "mineplex.com, hypixel.com, pixelmonmc.com";
+}
+
+function myFunction2() {
+  document.getElementById("clickme2").innerHTML = "Node, Wizcraft";
+}
+
+function myFunction3() {
+  document.getElementById("clickme3").innerHTML = "No free accounts available";
+}
+
+function myFunction4() {
+  document.getElementById("clickme4").innerHTML = "https://minecraft.net/da-dk/";
+}
+
+function letsmine() {
+  document.getElementById("letsmine").innerHTML = "lets mine!";
+}
